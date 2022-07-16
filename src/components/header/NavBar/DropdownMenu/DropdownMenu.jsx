56bc9587a4def1b4e630c1d570f108621ece7cc4
HoutarouM <maryan.goroshko@ukr.net>
2022-07-16 18:48:24 +0200

    add doc for components

--- src/components/header/NavBar/DropdownMenu/DropdownMenu.jsx
+++ src/components/header/NavBar/DropdownMenu/DropdownMenu.jsx
@@ -3,6 +3,11 @@ import React from 'react'
 // styles
 import styles from './DropDown.module.css'
 
+/**
+ * DropdownMenu is a function that returns a div with the className of DropdownMenu and the children of
+ * the props object.
+ * @returns A div with the className of DropdownMenu and the children of the DropdownMenu component.
+ */
 const DropdownMenu = (props) => {   
     return (
         <div className={styles.DropdownMenu}>
