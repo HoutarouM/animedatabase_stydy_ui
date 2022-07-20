e45e0f1797a48b8276241f2642bdaa004876aaad
HoutarouM <maryan.goroshko@ukr.net>
2022-07-20 09:01:20 +0200

    create header and footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,18 +1,18 @@
 import React from 'react'
 
 // styles
-import FooterStyle from './Footer.module.css'
+import './Footer.css'
 
 /**
  * This function returns a footer element with a paragraph element inside of it.
  * @returns A footer element with a paragraph element inside of it.
  */
 const Footer = () => {
-    return (
-        <footer className={FooterStyle.Footer}>
-            <p>Footer</p>
-        </footer>
-    )
+  return (
+    <footer>
+      <p>Footer</p>
+    </footer>
+  )
 }
 
 export default Footer
